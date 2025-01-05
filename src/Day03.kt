6bdf5089bc0aafac8b5bc9a32f5fa2f90dc4939b
Kent <kent.yuan@Gmail.com>
2025-01-05 23:19:22 +0100

    Day 03 ✅

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,17 +7,17 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) = input.map { line ->
+        line.trim().split(" +".toRegex()).map { it.toInt() }.let { (a, b, c) -> Triple(a, b, c) }
+    }.count { (a, b, c) -> a + b > c && a + c > b && b + c > a }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>): Int = input.map { line -> line.trim().split(" +".toRegex()).map { it.toInt() } }
+        .transpose().sumOf { col ->
+            col.chunked(3).count { (a, b, c) -> a + b > c && a + c > b && b + c > a }
+        }
 
     chkTestInput(Part1, testInput, 0) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
