9d0f68c5ef3d4a85adc6d4d655ca83a5f90e06bd
Kent <kent.yuan@Gmail.com>
2025-01-07 02:05:37 +0100

    Day 05 ✅

--- src/Day05.kt
+++ src/Day05.kt
@@ -6,18 +6,41 @@ fun main() {
 
     val input = readInput(today)
     val testInput = readTestInput(today)
-
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): String {
+        val re = """^00000[0-9a-f].*$""".toRegex()
+        val s = input.single()
+        var i = -1
+        return buildList {
+            while (true) {
+                val md5Str = "$s${i++}".md5()
+                if (re in md5Str) {
+                    add(md5Str[5])
+                    if (size == 8) break
+                }
+            }
+        }.joinChars()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): String {
+        val re = """^00000[0-7][0-9a-f].*$""".toRegex()
+        val s = input.single()
+        var i = -1
+        val result = MutableList(8) { 'x' }
+        while (true) {
+            val md5Str = "$s${i++}".md5()
+            if (re in md5Str) {
+                if (result[md5Str[5].digitToInt()] == 'x') {
+                    result[md5Str[5].digitToInt()] = md5Str[6]
+                }
+                if ('x' !in result) break
+            }
+        }
+        return result.joinChars()
     }
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, "18f47a30") { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, "05ace8e3") { part2(it) }
     solve(Part2, input) { part2(it) }
 }
