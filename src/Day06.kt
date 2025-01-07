1cb34e4922ca710dded76863a35d236d7074793a
Kent <kent.yuan@Gmail.com>
2025-01-07 02:12:13 +0100

    Day 06 ✅

--- src/Day06.kt
+++ src/Day06.kt
@@ -7,17 +7,17 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) = input.map { it.toList() }.transpose().map {
+        it.groupBy { it }.mapValues { it.value.size }.entries.maxByOrNull { it.value }!!.key
+    }.joinChars()
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>) = input.map { it.toList() }.transpose().map {
+        it.groupBy { it }.mapValues { it.value.size }.entries.minByOrNull { it.value }!!.key
+    }.joinChars()
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, "easter") { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, "advent") { part2(it) }
     solve(Part2, input) { part2(it) }
 }
