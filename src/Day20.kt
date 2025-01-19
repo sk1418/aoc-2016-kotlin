fef8632ea35aca9c967aeed3b66f76002aeabe7a
Kent <kent.yuan@Gmail.com>
2025-01-19 18:36:42 +0100

    Day 20 ✅

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,23 +1,48 @@
 import utils.*
 
-// https://adventofcode.com/2016/day/1x
+// https://adventofcode.com/2016/day/20
 fun main() {
     val today = "Day20"
 
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun toSortedPairs(input: List<String>) = input.map { it.substringBefore("-").toLong() to it.substringAfter("-").toLong() }.sortedBy { it.first }
+    fun part1(input: List<String>) = lowestAllowedIps(toSortedPairs(input))
+    fun part2(input: List<String>, upperBound: Long) = countAllowedIps(toSortedPairs(input), upperBound)
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 3) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
-    solve(Part2, input) { part2(it) }
+    chkTestInput(Part2, testInput, 2) { part2(it, 9) }
+    solve(Part2, input) { part2(it, UInt.MAX_VALUE.toLong()) }
+}
+
+private fun lowestAllowedIps(sortedPairs: List<Pair<Long, Long>>): Long {
+    var ip = 0L
+    var idx = 0
+    while (ip <= UInt.MAX_VALUE.toLong() && idx in sortedPairs.indices) {
+        if (ip >= sortedPairs[idx].first) {
+            if (ip <= sortedPairs[idx].second) ip = sortedPairs[idx].second + 1
+            idx++
+        } else return ip
+    }
+    return -1
+}
+
+private fun countAllowedIps(sortedPairs: List<Pair<Long, Long>>, upperBound: Long): Long {
+    var ip = 0L
+    var idx = 0
+    var result = 0L
+    while (ip <= upperBound && idx in sortedPairs.indices) {
+        if (ip >= sortedPairs[idx].first) {
+            if (ip <= sortedPairs[idx].second) ip = sortedPairs[idx].second + 1
+            idx++
+        } else {
+            result++
+            ip++
+        }
+    }
+    return result + upperBound - ip + 1
 }
